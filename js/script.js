d8ac30645bec8312603bd96a29b6ae6ab6123832
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-04-09 18:51:16 +0000

    af

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of a sphere.
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radiusOfSphere = parseFloat(document.getElementById('radius-of-sphere').value)
+
+  // process
+  const volumeOfSphere = 4 / 3 * 3.14 * radiusOfSphere
+
+  // output
+    document.getElementById('volume').innerHTML = 'Volume is: ' + volumeOfSphere.toFixed(2) + ' mm³'
+}
